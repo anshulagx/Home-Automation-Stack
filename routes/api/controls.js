220c04f6e1e9b949de2dcdfdb59293a831e8de66
Alok N <alokme123@gmail.com>
2021-03-10 12:25:22 +0530

    MQTT PubSub

--- routes/api/controls.js
+++ routes/api/controls.js
@@ -1,7 +1,31 @@
 //TODO: auth
 
 const express = require("express")
-
+var mqtt = require('mqtt')
+var options = {
+  port: 1883,
+  host: 'mqtt://65.1.164.121',
+  clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+  username: 'teamrookie',
+  password: 'benrowonahack',
+  keepalive: 60,
+  reconnectPeriod: 1000,
+  protocolId: 'MQIsdp',
+  protocolVersion: 3,
+  clean: true,
+  encoding: 'utf8'
+};
+var client = mqtt.connect('mqtt://65.1.164.121', options);
+client.on('connect', function() { // When connected
+    console.log('connected');
+    // subscribe to a topic
+    client.subscribe('testnode/publish', function() {
+        // when a message arrives, do something with it
+        client.on('message', function(topic, message, packet) {
+            console.log("Received '" + message + "' on '" + topic + "'");
+        });
+    });
+  });
 const router = express.Router()
 
 
@@ -20,8 +44,18 @@ router.post("/",(req, res) => {
   //   "extras":"adfs",
   //   "value":20
   // }
-
-
+  console.log(req.body);
+  if(req.body.id == 1){
+    console.log("reached");
+    var client  = mqtt.connect('mqtt://65.1.164.121',options)
+    client.on('connect', function() {
+      console.log('connected');
+      client.publish('testnode/action', 'switchON', function() {
+        console.log("ON Request Sent");
+        client.end();
+      });
+    });
+  }
   //req.body.id 
 
   //
