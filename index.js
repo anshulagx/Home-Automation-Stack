840c152485e41d37249c1df4dc9329d21d519a10
anshulUbuntu <anshulagx@gmail.com>
2021-03-12 10:51:10 +0530

    Heroku Integration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const profile = require('./routes/api/controls')
 
 
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express()
 
 app.use(express.json())
@@ -35,11 +35,15 @@ app.use(function (req, res, next) {
 });
 
 app.use("/api/controls", profile)
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+  });
 
 //For all unmatched routes
 app.use((req, res) => {
     res.status(400).json("Invalid API request");
 })
+
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Server started on port ${port}`))
